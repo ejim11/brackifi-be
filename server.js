8c7d0d14e86e05f7c9d79ff7494a60d7476249bb
ejim favour <favourejim56@gmail.com>
2024-03-13 20:06:45 +0100

    installed and enabled cors

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect(DB).then(() => {
   console.log('DB connection successfully established');
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
