bf19d0c5bcc398524f56c31efc80e0069e6731a7
ejim favour <favourejim56@gmail.com>
2024-03-15 23:36:14 +0100

    added the appError class and research route to the app

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const subscribersRouter = require('./routes/subscribersRoute');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+const researchRouter = require('./routes/researchRoute');
 
 const app = express();
 
@@ -21,5 +24,12 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/v1/subscribers', subscribersRouter);
+app.use('/api/v1/researches', researchRouter);
+
+app.use('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} at the moment`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
