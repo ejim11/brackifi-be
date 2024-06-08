3159e836790cbd241eebeada237fb1d68469ada8
ejim favour <favourejim56@gmail.com>
2024-06-08 13:13:21 +0100

    redsigned the docs and reports api

--- routes/docsAndReportsRoute.js
+++ routes/docsAndReportsRoute.js
@@ -4,11 +4,16 @@ const {
   getAllReports,
   deleteReport,
   createDocAndReport,
+  uploadReportDocFiles,
+  resizeReportImage,
 } = require('../controllers/admin/documentsAndReportsController');
 
 const router = express.Router();
 
-router.route('/').get(getAllReports).post(createDocAndReport);
+router
+  .route('/')
+  .get(getAllReports)
+  .post(uploadReportDocFiles, resizeReportImage, createDocAndReport);
 
 router.route('/:id').get(getReport).delete(deleteReport);
 
