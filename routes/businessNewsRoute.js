d4bdb7e9b860caefb30448eced83d94de6e5e342
ejim favour <favourejim56@gmail.com>
2024-06-09 17:47:34 +0100

    restructured the api for business news to accept images

--- routes/businessNewsRoute.js
+++ routes/businessNewsRoute.js
@@ -4,11 +4,16 @@ const {
   getAllBusinessNews,
   deleteBusinessNews,
   createBusinessNews,
+  uploadBusinessNewsPhoto,
+  resizeBusinessNewsPhoto,
 } = require('../controllers/admin/businessNewsController');
 
 const router = express.Router();
 
-router.route('/').get(getAllBusinessNews).post(createBusinessNews);
+router
+  .route('/')
+  .get(getAllBusinessNews)
+  .post(uploadBusinessNewsPhoto, resizeBusinessNewsPhoto, createBusinessNews);
 
 router.route('/:id').get(getBusinessNews).delete(deleteBusinessNews);
 
