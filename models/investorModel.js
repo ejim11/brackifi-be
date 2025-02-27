3fbee52037df48b9c5a01cbfba8b6ed6d3a15635
ejim11 <favourejim56@gmail.com>
2025-02-27 13:39:57 +0100

    ✅ added the ubitex id to the investor model

--- models/investorModel.js
+++ models/investorModel.js
@@ -14,6 +14,10 @@ const investorSchema = new mongoose.Schema(
       type: String,
       required: [true, 'Please provide an address'],
     },
+    ubitexId: {
+      type: String,
+      required: false,
+    },
     isLoginActivated: {
       type: Boolean,
       default: false,
